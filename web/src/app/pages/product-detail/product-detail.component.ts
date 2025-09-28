2c1ae617031cf701482dac2bec335b97d46dcf4e
jorgebls <iefaz.jorgebedoya@gmail.com>
2025-09-28 01:56:14 -0500

    feat: product detail page renders product data from service

--- web/src/app/pages/product-detail/product-detail.component.ts
+++ web/src/app/pages/product-detail/product-detail.component.ts
@@ -1,15 +1,25 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ProductsService } from '../../core/services/products.service';
+import { Product } from '../../shared/models/product.model';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent {
   private route = inject(ActivatedRoute);
-  productId = this.route.snapshot.paramMap.get('id'); // por ahora solo lo mostramos
+  private products = inject(ProductsService);
+
+  product: Product | undefined;
+
+  async ngOnInit() {
+    await this.products.seedIfEmpty();
+    const id = this.route.snapshot.paramMap.get('id')!;
+    this.product = this.products.getById(id);
+  }
 }
