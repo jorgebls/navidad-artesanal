4e4e93937e83a7f3cb14ca9294a5e4c53ea036a9
jorgebls <iefaz.jorgebedoya@gmail.com>
2025-09-28 00:42:12 -0500

    feat: add Cart page and route

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
-  { path: 'catalogo', loadComponent: () => import('./pages/catalog/catalog.component').then(m => m.CatalogComponent) },
-  { path: 'producto/:id', loadComponent: () => import('./pages/product-detail/product-detail.component').then(m => m.ProductDetailComponent) },
-];
+    { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+    { path: 'catalogo', loadComponent: () => import('./pages/catalog/catalog.component').then(m => m.CatalogComponent) },
+    { path: 'producto/:id', loadComponent: () => import('./pages/product-detail/product-detail.component').then(m => m.ProductDetailComponent) },
+    { path: 'carrito', loadComponent: () => import('./pages/cart/cart.component').then(m => m.CartComponent) },
+  ];
