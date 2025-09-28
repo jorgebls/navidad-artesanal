0ec4218fe5353398b31916b87a8a6f2af05614a5
jorgebls <iefaz.jorgebedoya@gmail.com>
2025-09-28 01:53:47 -0500

    feat: list products on /catalogo using ProductsService

--- web/src/app/pages/catalog/catalog.component.ts
+++ web/src/app/pages/catalog/catalog.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ProductsService } from '../../core/services/products.service';
+import { Product } from '../../shared/models/product.model';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './catalog.component.html',
-  styleUrl: './catalog.component.scss'
+  styleUrls: ['./catalog.component.scss']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  private svc = inject(ProductsService);
+  products: Product[] = [];
 
+  async ngOnInit() {
+    await this.svc.seedIfEmpty();    
+    this.products = this.svc.getAll();
+    console.log('products:', this.products);
+  }
+
+  onImgError(ev: Event) {
+    (ev.target as HTMLImageElement).src = 'assets/img/images.jpg';
+  }
 }
