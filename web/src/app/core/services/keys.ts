4caf352f2521b426bef79505aa824cd383130148
jorgebls <iefaz.jorgebedoya@gmail.com>
2025-09-28 00:57:25 -0500

    chore: StorageService for localStorage + LS keys

---
+++ web/src/app/core/services/keys.ts
@@ -0,0 +1,7 @@
+export const LS = {
+    USER_CURRENT: 'NA_USER_CURRENT', // usuario logueado
+    USERS: 'NA_USERS',               // lista de usuarios
+    PRODUCTS: 'NA_PRODUCTS',         // catálogo
+    CART: 'NA_CART',                 // carrito
+    ORDERS: 'NA_ORDERS'              // pedidos simulados
+  } as const;
